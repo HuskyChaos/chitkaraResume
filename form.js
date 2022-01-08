4d3d6cb17142a0a75505ed3c924ced0d0a2f3955
Vikash Kumar <vikashkumar.cse19@chitkarauniversity.edi.in>
2022-01-08 12:53:55 +0530

    2nd Commit

--- form.js
+++ form.js
@@ -2,26 +2,33 @@ window.onload = () => {
 
     var info = JSON.parse(window.localStorage.getItem('info'));
 
-    let name = document.getElementById('name');
-    let add1 = document.getElementById('add1');
-    let add2 = document.getElementById('add2');
-    let city = document.getElementById('city');
-    let state = document.getElementById('state');
-    let pin = document.getElementById('pin');
-    let phone = document.getElementById('phone');
-    let email = document.getElementById('email');
-    let li = document.getElementById('li');
-    let git = document.getElementById('git');
+    console.log(info);
 
-    name.innerHTML = info.name;
-    add1.innerHTML = info.add1;
-    add2.innerHTML = info.add2;
-    city.innerHTML = info.city;
-    state.innerHTML = info.state;
-    pin.innerHTML = info.pinCode;
-    phone.innerHTML = info.contact;
-    email.innerHTML = info.email;
-    li.innerHTML = info.li;
-    git.innerHTML = info.github;
-    
+    document.getElementsByClassName('name')[0].innerHTML = info.name;
+    document.getElementsByClassName('name')[1].innerHTML = info.name;
+    document.getElementById('add1').innerHTML = info.add1;
+    document.getElementById('add2').innerHTML = info.add2;
+    document.getElementById('city').innerHTML = info.city;
+    document.getElementById('state').innerHTML = info.state;
+    document.getElementById('pin').innerHTML = info.pinCode;
+    document.getElementById('phone').innerHTML = info.contact;
+    document.getElementById('email').innerHTML = info.email;
+    document.getElementById('li').innerHTML = info.li;
+    document.getElementById('git').innerHTML = info.github;
+    document.getElementById('q11').innerHTML = info.q1.q11;
+    document.getElementById('q12').innerHTML = info.q1.q12;
+    document.getElementById('q13').innerHTML = info.q1.q13;
+    document.getElementById('q14').innerHTML = info.q1.q14;
+    document.getElementById('q15').innerHTML = info.q1.q15;
+    document.getElementById('q21').innerHTML = info.q2.q21;
+    document.getElementById('q22').innerHTML = info.q2.q22;
+    document.getElementById('q23').innerHTML = info.q2.q23;
+    document.getElementById('q24').innerHTML = info.q2.q24;
+    document.getElementById('q25').innerHTML = info.q2.q25;
+    document.getElementById('q31').innerHTML = info.q3.q31;
+    document.getElementById('q32').innerHTML = info.q3.q32;
+    document.getElementById('q33').innerHTML = info.q3.q33;
+    document.getElementById('q34').innerHTML = info.q3.q34;
+    document.getElementById('q35').innerHTML = info.q3.q35;
+
 }
--- index.js
+++ index.js
@@ -2,20 +2,75 @@ window.onload = () => {
 
     let submit = document.getElementById('submit');
 
-    let info = {
-        name: null,
-        add1: null,
-        add2: null,
-        city: null,
-        state: null,
-        pinCode: null,
-        contact: null,
-        email: null,
-        li: null,
-        github: null
-    };
+    let info = JSON.parse(window.localStorage.getItem('info'));
+
+    if (info === null) {
+
+        info = { name: null, add1: null, add2: null, city: null,
+            state: null, pinCode: null, contact: null, email: null,
+            li: null, github: null, q1: {
+                q11: null,
+                q12: null,
+                q13: null,
+                q14: null,
+                q15: null
+            },
+            q2: {
+                q21: null,
+                q22: null,
+                q23: null,
+                q24: null,
+                q25: null
+            },
+            q3: {
+                q31: null,
+                q32: null,
+                q33: null,
+                q34: null,
+                q35: null
+            },
+            
+        };
+
+        console.log(info);
+
+    }
+
+    else {
+
+        document.getElementById('add1').value = info.add1;
+        document.getElementById('add2').value = info.add2;
+        document.getElementById('city').value = info.city;
+        document.getElementById('contact').value = info.contact;
+        document.getElementById('email').value = info.email;
+        document.getElementById('git').value = info.github;
+        document.getElementById('li').value = info.li;
+        document.getElementById('name').value = info.name;
+        document.getElementById('pin').value = info.pinCode;
+        document.getElementById('q11').value = info.q1.q11;
+        document.getElementById('q12').value = info.q1.q12;
+        document.getElementById('q13').value = info.q1.q13;
+        document.getElementById('q14').value = info.q1.q14;
+        document.getElementById('q15').value = info.q1.q15;
+        document.getElementById('q21').value = info.q2.q21;
+        document.getElementById('q22').value = info.q2.q22;
+        document.getElementById('q23').value = info.q2.q23;
+        document.getElementById('q24').value = info.q2.q24;
+        document.getElementById('q25').value = info.q2.q25;
+        document.getElementById('q31').value = info.q3.q31;
+        document.getElementById('q32').value = info.q3.q32;
+        document.getElementById('q33').value = info.q3.q33;
+        document.getElementById('q34').value = info.q3.q34;
+        document.getElementById('q35').value = info.q3.q35;
+        document.getElementById('state').value = info.state;
+
+    }
+
+
 
     submit.onclick = () => {
+
+        window.localStorage.removeItem("info");
 
         info.add1 = document.getElementById('add1').value;
         info.add2 = document.getElementById('add2').value;
@@ -26,6 +81,21 @@ window.onload = () => {
         info.li = document.getElementById('li').value;
         info.name = document.getElementById('name').value;
         info.pinCode = document.getElementById('pin').value;
+        info.q1.q11 = document.getElementById('q11').value;
+        info.q1.q12 = document.getElementById('q12').value;
+        info.q1.q13 = document.getElementById('q13').value;
+        info.q1.q14 = document.getElementById('q14').value;
+        info.q1.q15 = document.getElementById('q15').value;
+        info.q2.q21 = document.getElementById('q21').value;
+        info.q2.q22 = document.getElementById('q22').value;
+        info.q2.q23 = document.getElementById('q23').value;
+        info.q2.q24 = document.getElementById('q24').value;
+        info.q2.q25 = document.getElementById('q25').value;
+        info.q3.q31 = document.getElementById('q31').value;
+        info.q3.q32 = document.getElementById('q32').value;
+        info.q3.q33 = document.getElementById('q33').value;
+        info.q3.q34 = document.getElementById('q34').value;
+        info.q3.q35 = document.getElementById('q35').value;
         info.state = document.getElementById('state').value;
         
         window.localStorage.setItem('info', JSON.stringify(info));
